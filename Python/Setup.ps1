31b970b592e3d6954128d5bf59af7937c6e2b86f
Philsie <psiegerist.gaming@gmail.com>
2024-11-12 21:17:20 +0100

    fixed Python/Setup.ps1 when no .venv exists

--- Python/Setup.ps1
+++ Python/Setup.ps1
@@ -1,4 +1,6 @@
-rm .venv -Recurse -Force -Confirm:$false
+if(Test-Path -Path ./.venv){
+	rm .venv -Recurse -Force -Confirm:$false
+}
 python -m venv .venv
 .\.venv\Scripts\Activate.ps1
 pip install -r requirements.txt
